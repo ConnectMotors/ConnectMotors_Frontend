d5c29ed1dfb5f1c3381625a96279d418d347efc4
Matheus Henrique <matheushale@gmail.com>
2025-04-08 17:05:29 -0400

    criação de variáveis css para cores e font-family

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -8,12 +8,30 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    
+    font-family: var(--fonte-principal);
   }
 
   a {
     text-decoration: none;
     color: inherit;
+  }
+
+  :root{
+    --fonte-principal: "Poppins", Arial, sans-serif;
+    --branco: #ffffff;
+    --preto: #000000;
+    --azul-claro: #B6B9FF;
+    --cinza-background: #DDE0E2;
+    --cinza-1 : #f3f3f3;
+    --cinza-2 : #d9d9d9;
+    --cinza-3 : #a4a4a4;
+    --cinza-4 : #8c8c8c;
+    --cinza-5 : #565656;
+    --cinza-6 : #313131;
+    --azul-1 : #cbd3d8;
+    --azul-2 : #b6b9ff;
+    --azul-3 : #6b6eff;
+    --azul-4 : #0004ff;
   }
 `
 
