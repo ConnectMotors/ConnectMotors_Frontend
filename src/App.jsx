cf461e4140b98dd7a438c86938839bea35430a80
Matheus Henrique <matheushale@gmail.com>
2025-04-26 11:08:55 -0400

    atualizando o app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from "react";
-import ListagemVeiculos from "./components/ListagemVeiculos/ListagemVeiculos";
+
 
 function App() {
   return (
     <>
-      <ListagemVeiculos />
+     
     </>
   );
 }
