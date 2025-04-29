6e5bdadc44f000940d4833e78ea2ce3f5b28efc6
Matheus Henrique <matheushale@gmail.com>
2025-04-29 19:58:07 -0400

    Ajustando o container do componente ListagemVeiculos

--- src/components/ListagemVeiculos/ListagemVeiculos.jsx
+++ src/components/ListagemVeiculos/ListagemVeiculos.jsx
@@ -1,30 +1,32 @@
-import { ListaCards } from "./ListagemVeiculos.styles";
+import { Container, ListaCards } from "./ListagemVeiculos.styles";
 import CardAnuncio from "../Card-anuncio/Card-anuncio";
 
-export default function ListagemVeiculos({ veiculos = []}) {
+export default function ListagemVeiculos({ veiculos = [] }) {
   return (
-    <ListaCards className="lista-cards">
-      {veiculos.map((veiculo, index) => (
-        <li key={index}>
-          <CardAnuncio
-            id={veiculo.id} 
-            nomeVeiculo={veiculo.nomeVeiculo}
-            descricao={veiculo.descricao}
-            valor={veiculo.valor}
-            anoFabricacao={veiculo.anoFabricacao}
-            anoModelo={veiculo.anoModelo}
-            km={veiculo.km}
-            cidade={veiculo.cidade}
-            estado={veiculo.estado}
-            fabricante={veiculo.fabricante}
-            motor={veiculo.motor}
-            versao={veiculo.versao}
-            combustivel={veiculo.combustivel}
-            fotoPrincipal={veiculo.fotoPrincipal}
-            fotoSecundaria={veiculo.fotoSecundaria}
-          />
-        </li>
-      ))}
-    </ListaCards>
+    <Container>
+      <ListaCards>
+        {veiculos.map((veiculo, index) => (
+          <li key={index}>
+            <CardAnuncio
+              id={veiculo.id}
+              nomeVeiculo={veiculo.nomeVeiculo}
+              descricao={veiculo.descricao}
+              valor={veiculo.valor}
+              anoFabricacao={veiculo.anoFabricacao}
+              anoModelo={veiculo.anoModelo}
+              km={veiculo.km}
+              cidade={veiculo.cidade}
+              estado={veiculo.estado}
+              fabricante={veiculo.fabricante}
+              motor={veiculo.motor}
+              versao={veiculo.versao}
+              combustivel={veiculo.combustivel}
+              fotoPrincipal={veiculo.fotoPrincipal}
+              fotoSecundaria={veiculo.fotoSecundaria}
+            />
+          </li>
+        ))}
+      </ListaCards>
+    </Container>
   );
 }
--- src/components/ListagemVeiculos/ListagemVeiculos.styles.js
+++ src/components/ListagemVeiculos/ListagemVeiculos.styles.js
@@ -1,5 +1,23 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+  width: 100%;
+  max-width: 1440px;
+  padding: 0 20px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  @media (max-width: 1440px) {
+    max-width: 1200px;
+  }
+
+  @media (max-width: 768px) {
+    padding: 0 12px;
+  }
+`;
+
 export const ListaCards = styled.ul`
   display: flex;
   flex-wrap: wrap;
@@ -7,6 +25,6 @@ export const ListaCards = styled.ul`
   padding: 40px 0;
   justify-content: center;
   list-style: none;
-  margin: 0 auto;
-  max-width: 1440px;
+  width: 100%;
+  margin: 0;
 `;
